598881081dcd149cef41499d362eb1ff2f32a857
ziming0v0 <1563238589@qq.com>
2026-07-13 15:39:53 +0800

    初始化工作区和健康查询点

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{serve, Router};
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/health",get(|| async { "healthy" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    serve(listener, app).await?;
+    
+    Ok(())
 }
